04089babc1f74173ec614bca9ea67e8779c693f9
mscline <mychiloscline@gmail.com>
2015-01-21 15:02:27 -0800

    basic pie chart completed

--- PieChart/ViewController.swift
+++ PieChart/ViewController.swift
@@ -26,45 +26,158 @@ import UIKit
 
 class ViewController: UIViewController {
 
+        // INSTANCE VARIABLES
+
+        //var arrayOfPieSlices:[PieSlice]?
+    let lineWidth:Float = 2.0
+
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
+        let view = createUIViewWithEmbeddedPieChart(arrayOfPieSlices:createSampleData(), desiredHeightAndWidthOfView:200)
+        self.view.addSubview(view)
+        view.backgroundColor = UIColor.grayColor()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createSampleData()->(Array<PieSlice>){
+
+        let a = PieSlice(titleForLegend: "a", colorOfPieSlice: UIColor.blueColor(), positionOfPieSlice_indexNumber: 0, theTotal: 90)
+        let b = PieSlice(titleForLegend: "b", colorOfPieSlice: UIColor.purpleColor(), positionOfPieSlice_indexNumber: 0, theTotal: 30)
+        let c = PieSlice(titleForLegend: "c", colorOfPieSlice: UIColor.brownColor(), positionOfPieSlice_indexNumber: 0, theTotal: 30)
+        let c2 = PieSlice(titleForLegend: "c", colorOfPieSlice: UIColor.redColor(), positionOfPieSlice_indexNumber: 0, theTotal: 30)
+        let d = PieSlice(titleForLegend: "d", colorOfPieSlice: UIColor.greenColor(), positionOfPieSlice_indexNumber: 0, theTotal: 100)
+        let d2 = PieSlice(titleForLegend: "d", colorOfPieSlice: UIColor.yellowColor(), positionOfPieSlice_indexNumber: 0, theTotal: 40)
+
+        // add to array
+        var arrayOfPieSlicesToDisplay:[PieSlice] = Array()
+
+        arrayOfPieSlicesToDisplay.append(a)
+        arrayOfPieSlicesToDisplay.append(b)
+        arrayOfPieSlicesToDisplay.append(c)
+        arrayOfPieSlicesToDisplay.append(c2)
+        arrayOfPieSlicesToDisplay.append(d)
+        arrayOfPieSlicesToDisplay.append(d2)
+
+        return arrayOfPieSlicesToDisplay
+
     }
 
+    // MARK: CREATE PIE CHART
+
+    func createUIViewWithEmbeddedPieChart(#arrayOfPieSlices:Array<PieSlice>!, desiredHeightAndWidthOfView:CGFloat)->(UIView){
+
+        let view = UIView(frame: CGRectMake(0, 0, desiredHeightAndWidthOfView, desiredHeightAndWidthOfView))
+
+        // make sure pie slices sorted properly
+//        let sortedArray = arrayOfPieSlices.sorted { (a, b) -> Bool in
+//
+//            return a.indexNumberTellingOrderToDisplayPiePieces < b.indexNumberTellingOrderToDisplayPiePieces
+//        }
+
+        // calculate total value of all pie slices
+        let total = addupTotalValueOfAllPieSlices(arrayOfPiceSlices: arrayOfPieSlices)
+
+        // for each pie slice
+        var startAtAngle_inRadians:Float = 0
+
+        for slice in arrayOfPieSlices {
+
+            let desiredHeightAndWidthOfView_float = Float(desiredHeightAndWidthOfView)
+            let pieTotalValue = Float(slice.theTotal)
+            let angleOfPie = (pieTotalValue / total) * 2 * 3.14159
+
+            let sliceOfPie = createSliceOfPie(heightAndWidthOfView:desiredHeightAndWidthOfView_float, startAngleInRadians: startAtAngle_inRadians, angleInRadians: angleOfPie, color:slice.colorOfPieSlice)
+            view.layer.addSublayer(sliceOfPie)
+
+            startAtAngle_inRadians = startAtAngle_inRadians + angleOfPie
+
+        }
+
+        return view
+    }
+
+    func addupTotalValueOfAllPieSlices(#arrayOfPiceSlices:Array<PieSlice>)->(Float){
+
+        var total:Float = 0.0
+
+        for slice in arrayOfPiceSlices {
+
+            total = total + slice.theTotal.floatValue
+        }
+
+        return total
+    }
+
+    func createSliceOfPie(#heightAndWidthOfView:Float, startAngleInRadians:Float, angleInRadians:Float, color:UIColor)->(CAShapeLayer){
+
+
+        // APPLE DRAWS THE UNIT CIRCLE BACKWARDS, SO FLIP SIGNS
+        var startAngle_inRadians = -startAngleInRadians
+
+        // MAKE BASIC CALCULATIONS FOR OUR CIRCLE
+        let radius = (heightAndWidthOfView - lineWidth)/2
+        let finalAngle = startAngle_inRadians - angleInRadians  // FLIP SIGN
+
+        let xCoordPointOnArc = heightAndWidthOfView/2 + radius * cosf(startAngle_inRadians)
+        let yCoordPointOnArc = heightAndWidthOfView/2 + radius * sinf(startAngle_inRadians)
+
+        // convert floats to CGFloats (arrggghhh...)
+        let xCoordPointOnArc_cgfloat = CGFloat(xCoordPointOnArc)
+        let yCoordPointOnArc_cgfloat = CGFloat(yCoordPointOnArc)
+        let startAngle_cgfloat = CGFloat(startAngle_inRadians)
+        let finalAngle_cgfloat = CGFloat(finalAngle)
+        let angle_cgfloat = CGFloat(angleInRadians)
+
+        // create additional variables to work with
+        let radius_cgfloat = CGFloat(radius)
+        let center_cgFloat = CGPointMake(CGFloat(heightAndWidthOfView/2), CGFloat(heightAndWidthOfView/2))
+        let xyCoordPointOnArc = CGPointMake(xCoordPointOnArc_cgfloat, yCoordPointOnArc_cgfloat)
+
+
+        // CREATE LAYER
+        let shapeLayer = CAShapeLayer()
+
+        // set color and line settings
+        shapeLayer.fillColor = color.CGColor
+        shapeLayer.strokeColor = UIColor.blackColor().CGColor
+        shapeLayer.lineWidth = 2.0
+
+
+        // BUILD SHAPE FROM LINE SEGEMENTS (ie a Bezier Path)
+        let connectedLineSegments = UIBezierPath()
+
+            // 1) set start point = center
+            connectedLineSegments.moveToPoint(center_cgFloat)
+
+            // 2) connect with point on our circle
+            connectedLineSegments.addLineToPoint(xyCoordPointOnArc)
+
+            // 3) draw arc
+            connectedLineSegments.addArcWithCenter(center_cgFloat, radius:radius_cgfloat, startAngle:startAngle_cgfloat, endAngle: finalAngle_cgfloat, clockwise: false)
+
+            // 4) draw line from last point back to the center
+            connectedLineSegments.closePath()  // rather than calling addLine, we could use close path which will draw a line to connect our line with our start point
+
+
+        // use our Bezier Path as a mask
+        shapeLayer.path = connectedLineSegments.CGPath
+
+        return shapeLayer
+    }
+
+    // MARK: CODING
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
 }
 
 
-/*
 
--(CAShapeLayer *)createPieSlice {
-    CAShapeLayer *slice = [CAShapeLayer layer];
-    slice.fillColor = [UIColor redColor].CGColor;
-    slice.strokeColor = [UIColor blackColor].CGColor;
-    slice.lineWidth = 3.0;
-
-    CGFloat angle = DEG2RAD(-60.0);
-    CGPoint center = CGPointMake(100.0, 100.0);
-    CGFloat radius = 100.0;
-
-    UIBezierPath *piePath = [UIBezierPath bezierPath];
-    [piePath moveToPoint:center];
-
-    [piePath addLineToPoint:CGPointMake(center.x + radius * cosf(angle), center.y + radius * sinf(angle))];
-
-    [piePath addArcWithCenter:center radius:radius startAngle:angle endAngle:DEG2RAD(60.0) clockwise:YES];
-
-    //   [piePath addLineToPoint:center];
-    [piePath closePath]; // this will automatically add a straight line to the center
-    slice.path = piePath.CGPath;
-    
-    return slice;
-}
-
-*/
 
---
+++ PieSlice.swift
@@ -0,0 +1,31 @@
+//
+//  PieSlice.swift
+//  PieChart
+//
+//  Created by xcode on 1/21/15.
+//  Copyright (c) 2015 MSCline. All rights reserved.
+//
+
+import UIKit
+
+class PieSlice: NSObject {
+
+        // INSTANCE VARIABLES
+
+        var titleForLegend:NSString!
+        var theTotal:NSNumber!
+
+        var colorOfPieSlice:UIColor!
+        var indexNumberTellingOrderToDisplayPiePieces:NSInteger!
+
+
+    init(titleForLegend:NSString, colorOfPieSlice:UIColor, positionOfPieSlice_indexNumber:NSInteger, theTotal:NSNumber) {
+
+        self.titleForLegend = titleForLegend ?? ""
+        self.colorOfPieSlice = colorOfPieSlice ?? UIColor.blueColor()
+        self.indexNumberTellingOrderToDisplayPiePieces = positionOfPieSlice_indexNumber ?? 0
+        self.theTotal = theTotal ?? 0
+
+    }
+
+}
